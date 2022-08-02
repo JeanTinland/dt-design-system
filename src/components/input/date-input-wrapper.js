8b508ee8cd2efa4accbac96a079d81dfc305e631
Jean Tinland <jean.tinland@gmail.com>
2022-08-02 10:43:09 +0200

    Fix date input calendar positioning + bump 0.9.16

--- src/components/input/date-input-wrapper.js
+++ src/components/input/date-input-wrapper.js
@@ -4,11 +4,14 @@ import css from "./date-input-wrapper.module.css";
 
 const DateInputWrapper = ({ container, children }) => {
   const { scrollX, scrollY } = window;
+  const { top: YOffset, left: XOffset } = document.body.getBoundingClientRect();
   const { top, left, height } = container.getBoundingClientRect();
+
   const styles = {
-    top: top + height + scrollY,
-    left: left + scrollX,
+    top: top + height + scrollY - YOffset,
+    left: left + scrollX - XOffset,
   };
+
   return ReactDOM.createPortal(
     <div className={css.dateInputWrapper} style={styles}>
       {children}
